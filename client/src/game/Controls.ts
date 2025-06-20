a0325829dfe747e3765161b2c8983cfedd2f96aa
Klinola <KelinerALOC@outlook.com>
2025-06-20 10:44:24 +0800

    fix(client): fix joystick initialization in Controls component

--- client/src/game/Controls.ts
+++ client/src/game/Controls.ts
@@ -37,12 +37,10 @@ export class Controls {
       game: { input },
     } = this;
     if (this.game.isMobile) {
-      // @ts-ignore
-      this.joystick = this.game.plugins
-        .get('rexVirtualJoystick')
-        ?.add(this.game.hud.scene, {
-          radius: 130,
-        }) as VirtualJoyStick;
+      const joystickPlugin = this.game.plugins.get('rexVirtualJoystick') as any;
+      this.joystick = joystickPlugin?.add(this.game.hud.scene, {
+        radius: 130,
+      }) as VirtualJoyStick;
       this.joystick.on('pointerdown', (pointer: any) => {
         this.joystickPointer = pointer;
       });
