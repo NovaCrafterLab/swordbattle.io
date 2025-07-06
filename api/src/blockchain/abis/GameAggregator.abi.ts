885b30fddc43cd937cf84003167bee16f0acb4d5
damianxiao <damianxiao22@gmail.com>
2025-07-06 15:07:33 +0800

    feat: update abis dir

--- api/src/blockchain/abis/GameAggregator.abi.ts
+++ api/src/blockchain/abis/GameAggregator.abi.ts
@@ -1,4 +1,4 @@
 // GameAggregator ABI - 从统一ABI文件导入
-import * as GameAggregatorABI from '../../../../abis/GameAggregator.json';
+import * as GameAggregatorABI from './GameAggregator.json';
 
 export const GAME_AGGREGATOR_ABI = (GameAggregatorABI as any).abi;
